15a0d1e0574fff94430e0f534011b9e2c8f29000
emidioreb <emidioreboucas96@gmail.com>
2024-11-25 20:10:06 -0300

    first commit

---
+++ src/main/java/com/example/bffimd/BffImdApplication.java
@@ -0,0 +1,13 @@
+package com.example.bffimd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffImdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffImdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bffimd/BffImdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bffimd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffImdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
